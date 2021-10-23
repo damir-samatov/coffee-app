1cad682fb69f0830d9e3d70378359757ed52c270
Damir Samatov <damirsamatofff@gmail.com>
2021-10-23 14:30:10 +0500

    contacts page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Nav from "components/Nav";
 import MainPage from "components/MainPage";
 import Footer from "components/Footer/Footer";
-import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { HashRouter as Router, Link, Route, Switch } from "react-router-dom";
 import ContactsPage from "components/ContactsPage/ContactsPage";
 export default function App() {
     return (
