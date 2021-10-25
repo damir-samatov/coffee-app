8bf44b33378898a25d0e1fbcc2aece04b2ff2366
Damir Samatov <damirsamatofff@gmail.com>
2021-10-26 01:16:46 +0500

    rwewerwrwerwe

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Nav from "components/Nav";
 import MainPage from "components/MainPage";
 import Footer from "components/Footer/Footer";
-import { HashRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import ContactsPage from "components/ContactsPage/ContactsPage";
 import BlogPage from "components/BlogPage/BlogPage";
 export default function App() {
