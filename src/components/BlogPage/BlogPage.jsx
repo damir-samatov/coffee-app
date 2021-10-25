470567abc74a77d9e68a119f4a327729e8668687
Damir Samatov <damirsamatofff@gmail.com>
2021-10-25 20:46:50 +0500

    added dynamic content to blog page

--- src/components/BlogPage/BlogPage.jsx
+++ src/components/BlogPage/BlogPage.jsx
@@ -1,6 +1,13 @@
 import { useEffect } from "react";
+import { useState } from "react/cjs/react.development";
 import s from "./BlogPage.module.scss";
+import icons from "./icons";
 export default function BlogPage() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    console.log("general render");
+    const iconClickHandler = (i) => {
+        setActiveIndex(i);
+    };
     useEffect(() => {
         window.scroll({
             top: 0,
@@ -18,6 +25,41 @@ export default function BlogPage() {
                     <button className={s.button}>Обучение</button>
                     <button className={s.button}>Новости</button>
                     <button className={s.button}>Частые вопросы</button>
+                </div>
+                <div className={s.main}>
+                    <div className={s.makingCoffee}>
+                        <h4 className={s.heading}>
+                            Приготовление кофе разными способами
+                        </h4>
+                        <div className={s.icons}>
+                            {icons.map((item, i) => {
+                                return (
+                                    <div
+                                        key={`${item.name}_${i}`}
+                                        className={
+                                            i === activeIndex
+                                                ? `${s.icon} ${s.active}`
+                                                : s.icon
+                                        }
+                                        onClick={() => iconClickHandler(i)}
+                                    >
+                                        <h6 className={s.name}>{item.name}</h6>
+                                        <div className={s.image}>
+                                            {item.icon}
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                        <div className={s.main}>
+                            <h4 className={s.heading}>
+                                {icons[activeIndex].subSection}
+                            </h4>
+                            <p className={s.paragraph}>
+                                {icons[activeIndex].text}
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
